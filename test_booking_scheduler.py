49a1edf864cef77534db88f7a99e061a6f1e06b3
dasol.han <koda911101@gmail.com>
2024-06-14 11:32:46 +0900

    [feature] 5

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -11,25 +11,30 @@ CAPACITY_PER_HOUR = 3
 
 
 class BookingSchedulerTest(unittest.TestCase):
+    def setUp(self):
+        self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
 
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
         not_on_the_hour = datetime.strptime("2024/06/14 09:05", "%Y/%m/%d %H:%M")
         schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
-        booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
 
         with self.assertRaises(ValueError):
-            booking_scheduler.add_schedule(schedule)
+            self.booking_scheduler.add_schedule(schedule)
 
     def test_예약은_정시에만_가능하다_정시인_경우_예약가능(self):
         schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
-        booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
-
-        booking_scheduler.add_schedule(schedule)
-
-        self.assertTrue(booking_scheduler.has_schedule(schedule))
+        self.booking_scheduler.add_schedule(schedule)
+        self.assertTrue(self.booking_scheduler.has_schedule(schedule))
 
     def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생(self):
-        pass
+        schedule = Schedule(ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER)
+        self.booking_scheduler.add_schedule(schedule)
+
+        with self.assertRaises(ValueError) as context:
+            new_schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+            self.booking_scheduler.add_schedule(new_schedule)
+
+        self.assertEquals("Number of people is over restaurant capacity per hour", str(context.exception))
 
     def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공(self):
         pass

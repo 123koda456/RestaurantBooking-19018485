12e5fc18bc9d29dcd50cd4404ff28d297bfeecf2
dasol.han <koda911101@gmail.com>
2024-06-14 11:59:34 +0900

    [feature] 12

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -18,6 +18,8 @@ class BookingSchedulerTest(unittest.TestCase):
         self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
         self.testable_sms_sender = TestableSmsSender()
         self.booking_scheduler.set_sms_sender(self.testable_sms_sender)
+        self.testable_mail_sender = TestableMailSender()
+        self.booking_scheduler.set_mail_sender(self.testable_mail_sender)
 
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
         not_on_the_hour = datetime.strptime("2024/06/14 09:05", "%Y/%m/%d %H:%M")
@@ -59,22 +61,19 @@ class BookingSchedulerTest(unittest.TestCase):
         self.assertTrue(self.testable_sms_sender.is_send_method_is_called())
 
     def test_이메일이_없는_경우에는_이메일_미발송(self):
-        testable_mail_sender = TestableMailSender()
         schedule = Schedule(ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER)
-        self.booking_scheduler.set_mail_sender(testable_mail_sender)
 
         self.booking_scheduler.add_schedule(schedule)
-        self.assertEqual(testable_mail_sender.get_count_send_mail_is_called(), 0)
+
+        self.assertEqual(self.testable_mail_sender.get_count_send_mail_is_called(), 0)
         pass
 
     def test_이메일이_있는_경우에는_이메일_발송(self):
         customer_with_mail = Customer("Fake Name", "010-1234-5678", "test@test.com")
-        testable_mail_sender = TestableMailSender()
         schedule = Schedule(ON_THE_HOUR, CAPACITY_PER_HOUR, customer_with_mail)
-        self.booking_scheduler.set_mail_sender(testable_mail_sender)
 
         self.booking_scheduler.add_schedule(schedule)
-        self.assertEqual(testable_mail_sender.get_count_send_mail_is_called(), 1)
+        self.assertEqual(self.testable_mail_sender.get_count_send_mail_is_called(), 1)
         pass
 
     def test_현재날짜가_일요일인_경우_예약불가_예외처리(self):
